12dcde1e6328f208eb2c0ae0146d341ef82de9a4
hamflx <a@hamflx.cn>
2022-08-27 11:47:37 +0800

    initial commit

---
+++ src/error.rs
@@ -0,0 +1,23 @@
+use thiserror::Error;
+use windows_sys::Win32::Foundation::WIN32_ERROR;
+
+#[derive(Error, Debug)]
+pub enum InjectorError {
+    #[error("Win32 Error")]
+    Win32Error(WIN32_ERROR),
+    #[error("Bincode Error")]
+    BincodeError(bincode::Error),
+    #[error("Bincode Error")]
+    ArchMismatch(&'static str, &'static str),
+    #[error("Other Error")]
+    Other(String),
+}
+
+pub type InjectorResult<T> = Result<T, InjectorError>;
+
+#[macro_export]
+macro_rules! err {
+    ($($t:tt)+) => {
+        InjectorError::Other(format!($($t)+))
+    };
+}
